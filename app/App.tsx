a1d4cea1b29fbcb0da747069558f7c434a2c2558
Artyom Titov <justblender@ya.ru>
2023-08-23 23:38:14 +0300

    feat: edge-to-edge mode on Android

--- app/App.tsx
+++ app/App.tsx
@@ -1,9 +1,11 @@
+import { StatusBar } from "expo-status-bar";
 import { AppProvider } from "~/providers";
 import { AppNavigation } from "~/navigation";
 
 export default function App() {
   return (
     <AppProvider>
+      <StatusBar style="auto" />
       <AppNavigation />
     </AppProvider>
   );
