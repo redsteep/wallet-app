85e3aa3526dde0c400f48f57f77eeac14a03f0cf
Artyom Titov <justblender@ya.ru>
2023-09-07 00:43:03 +0300

    fix: fix annoying flicker on initial render of SafeAreaStack

--- app/src/components/safe-area-stack.tsx
+++ app/src/components/safe-area-stack.tsx
@@ -1,5 +1,47 @@
-import { SafeAreaView } from "react-native-safe-area-context";
+import { forwardRef, useMemo } from "react";
+import { StyleSheet, View, type DimensionValue, type ViewProps } from "react-native";
+import { useSafeAreaInsets, type Edge } from "react-native-safe-area-context";
 import { styled } from "tamagui";
+
+const allEdges: Edge[] = ["top", "left", "right", "bottom"];
+
+const propertySuffixMap: Record<Edge, string> = {
+  top: "Top",
+  left: "Left",
+  right: "Right",
+  bottom: "Bottom",
+};
+
+interface SafeAreaViewProps extends ViewProps {
+  mode?: "padding" | "margin";
+  edges?: Edge[];
+}
+
+export const SafeAreaView = forwardRef<View, SafeAreaViewProps>(
+  ({ style, mode = "padding", edges = allEdges, ...rest }, ref) => {
+    const insets = useSafeAreaInsets();
+
+    const containerStyle = useMemo(() => {
+      const flattenedStyle = StyleSheet.flatten(style) as Record<string, DimensionValue>;
+
+      return Object.fromEntries(
+        edges.map((edge) => {
+          const propertyName = `${mode}${propertySuffixMap[edge]}`;
+          const propertyValue = flattenedStyle[propertyName] ?? 0.0;
+
+          return [
+            propertyName,
+            typeof propertyValue === "number"
+              ? propertyValue + insets[edge]
+              : insets[edge],
+          ];
+        }),
+      );
+    }, [style, edges, mode, insets]);
+
+    return <View ref={ref} style={[style, containerStyle]} {...rest} />;
+  },
+);
 
 export const SafeAreaStack = styled(SafeAreaView, {
   flex: 1,
